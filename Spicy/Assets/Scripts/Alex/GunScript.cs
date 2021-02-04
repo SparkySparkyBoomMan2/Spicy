13494a65e0784e680b5ae699f67880cd05edfc8d
Alex Peña <alexpena5635@gmail.com>
2021-02-04 00:19:39 -0800

    Added Weapon for Demo + shooting
    Added a small weapons art pack (temporary) to show off for demo. Added to my prefabs, art, and scripts folder to create a gun that rotates towards the mouse and shoots bullets.  On impact, particle system is created and played.
    
    Also fixed the issue with the main player not being able to be seen. And added my gun prefab, as a child of the player in the scene, so that we can show it off.

---
+++ Spicy/Assets/Scripts/Alex/GunScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public GameObject bullet;
+    public SpriteRenderer gunRenderer;
+    
+    private bool AllowFire;
+    void Start()
+    {
+        AllowFire = true;
+    }
+
+    
+    void FixedUpdate()
+    {
+        AimGun();
+
+        if (Input.GetMouseButton(0) && AllowFire)
+        {
+            StartCoroutine(FireGun());
+        }
+    }
+
+    void AimGun()
+    {
+        var mousePos = Input.mousePosition;
+        mousePos.z = 5.23f;
+        var objectPos = Camera.main.WorldToScreenPoint(this.transform.position);
+        mousePos.x = mousePos.x - objectPos.x;
+        mousePos.y = mousePos.y - objectPos.y;
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+        if (transform.rotation.z > .7f || transform.rotation.z < -.7f)
+        {
+            gunRenderer.flipY = true;
+        }
+        else
+        {
+            gunRenderer.flipY = false;
+        }
+    }
+
+    IEnumerator FireGun()
+    {
+        AllowFire = false;
+        Vector3 rot = transform.GetChild(0).rotation.eulerAngles;
+        rot = new Vector3(rot.x, rot.y, rot.z + 90);
+
+        Instantiate(bullet, new Vector2 (transform.GetChild(0).position.x, transform.GetChild(0).position.y), Quaternion.Euler(rot));
+        yield return new WaitForSeconds(.50f);
+        AllowFire = true;
+        
+    }
+
+    
+}
---
+++ Spicy/Assets/Scripts/Alex/bulletScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public ParticleSystem BulletExplode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = new Vector3(transform.right.y, transform.right.x * -1, transform.right.z) * 10f;
+        //rb.velocity = new Vector3( transform.right.x, transform.right.y, transform.right.z) * 10f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log(other.gameObject.tag);
+
+        //if (other.gameObject.name == "TilemapPlatforms")
+        if (other.gameObject.name != "Player")
+        {
+            BulletImpact();
+        }
+    }
+
+    void BulletImpact()
+    {
+        //rb.velocity = new Vector2(rb.velocity.x, -2);
+        Instantiate(BulletExplode, new Vector2 (transform.position.x, transform.position.y), Quaternion.identity);
+        Destroy(gameObject);
+        //Destroy(BulletExplode);
+        Debug.Log("META");
+    }
+}
