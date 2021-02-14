fe71b4c3fc9746b09bbe3f7e0d43cec640f59334
David Waters <davidj2012@gmail.com>
2021-02-14 15:38:02 -0800

    Added Spawner and One-way Tilemap capablility
    Added scripts for spawning objects and affecting tiles as one way. Added a materials folder with a new physics material 2D for frictionless contact.

---
+++ Spicy/Assets/Scripts/David/OneWayPlatformDirectionChange.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This class effect does not take into account multiplayer settings.
+// Unsure how this behavior works if multiple players pressing UP, DOWN or JUMP at the same time
+public class OneWayPlatformDirectionChange : MonoBehaviour
+{
+    private PlatformEffector2D effector;
+    public float waitTime = 0.5f;
+
+    void Start()
+    {
+        effector = GetComponent<PlatformEffector2D>();
+    }
+
+    // May want to change this to work with just axis input instead of specific keys
+    // Especially important in the case of using a controller or touch over keyboard
+    void Update()
+    {
+        // If UP or DOWN are released, the wait time to flip the one-way platform direction resets
+        if (Input.GetButtonUp("Vertical"))
+        {
+            waitTime = 0.5f;
+        }
+
+        // This currently allows for both UP and DOWN to affect hold time for platform direction change
+        // Pressing|Holding|Releasing DOWN count down a timer to switch the one-way platform direction
+        if (Input.GetButton("Vertical"))
+        {
+            if (waitTime <= 0)
+            {
+                effector.rotationalOffset = 180f;
+                waitTime = 0.5f;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+
+        // If JUMP is pressed, the one-way platform direction resets to the default direction
+        if (Input.GetButtonDown("Jump"))
+        {
+            effector.rotationalOffset = 0;
+        }
+    }
+}
---
+++ Spicy/Assets/Scripts/David/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Spawns in the desired prefab to the location of the parent object
+public class Spawner : MonoBehaviour
+{
+    // Could include a variable that goes into the array and selects which object to spawn
+    // Would then have to update the spawnObject method
+    public GameObject[] spawnableObjects;
+    public Transform parent;
+
+
+    // Add more enum variables according to the different enemies, players, etc. that are made
+    [SerializeField]
+    private enum spawnable
+    {
+        PLAYER,
+        ENEMY,
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Need to add failure-check code
+        parent = GetComponent<Transform>();
+        SpawnObject();
+    }
+
+    void SpawnObject()
+    {
+        Instantiate(spawnableObjects[0], parent);
+
+    }
+}
