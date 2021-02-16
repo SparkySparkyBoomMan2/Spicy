92e0ebb64aedb9e0d3cf64a1ecaa3ff48fd59330
Nathaniel Palmer <61099489+napomj@users.noreply.github.com>
2021-02-15 20:51:09 -0800

    animate and script

---
+++ Spicy/Assets/Scripts/Nathaniel/Tomato_Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tomato_Enemy : MonoBehaviour
+{
+    int speed;
+    int damage;
+    int life;
+    float damageRadius;
+    float pursueRadius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
