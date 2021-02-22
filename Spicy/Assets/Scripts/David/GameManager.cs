f09e7546a6eb046559cce1b74fa29a3e947a13dd
David Waters <davidj2012@gmail.com>
2021-02-22 14:40:45 -0800

    Working on a Game Manager

---
+++ Spicy/Assets/Scripts/David/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // This class controls things like:
+    // - UI Elements such as score
+    // - Lives
+    // - Handling going in and out of menus
+
+    public GameObject Player;
+    public GameObject Level;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
