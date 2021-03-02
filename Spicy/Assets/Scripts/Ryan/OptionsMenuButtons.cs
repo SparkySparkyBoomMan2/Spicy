e6112744e5098477c08b2bf6c1a72965b64d8b1a
gellyboi <78063792+gellyboi@users.noreply.github.com>
2021-03-02 01:58:34 -0800

    Added OptionsMenu prefab & scene
    Scripting for buttons and sliders are a WIP

---
+++ Spicy/Assets/Scripts/Ryan/OptionsMenuButtons.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenuButtons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void musicVolUpdate(float vol)
+    {
+        Debug.Log(vol);
+    }
+    public void soundVolUpdate(float vol)
+    {
+        float volume = vol;
+        Debug.Log(volume);
+    }
+    public void OnValueChanged(float vol)
+    {
+        Debug.Log(vol);
+    }
+}
