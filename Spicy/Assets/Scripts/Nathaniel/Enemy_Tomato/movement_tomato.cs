8f5cb37ca2a6e61ebc4da3696e41c10bd303ed84
Nathaniel Palmer <61099489+napomj@users.noreply.github.com>
2021-03-03 22:19:02 -0800

    EnemysDropDown

--- Spicy/Assets/Scripts/Nathaniel/Enemy_Tomato/movement_tomato.cs
+++ Spicy/Assets/Scripts/Nathaniel/Enemy_Tomato/movement_tomato.cs
@@ -8,7 +8,9 @@ public class Movement_Tomato : MonoBehaviour
     private float walk_speed = 1.0f;
     bool moveRight = true;
     public Transform wallDetect;
-
+    public Transform groundDetect1;
+    public Transform groundDetect2;
+    Vector2 castDown = new Vector2(0, -2);
     // Update is called once per frame
     void Update()
     {
@@ -27,11 +29,22 @@ public class Movement_Tomato : MonoBehaviour
     {
         int dirFlag = 2;
         RaycastHit2D wallInfo = Physics2D.Raycast(wallDetect.position, Vector2.right, .01f);
-        RaycastHit2D groundInfo = Physics2D.Raycast(wallDetect.position, Vector2.down, .01f);
+        RaycastHit2D groundInfo1 = Physics2D.Raycast(groundDetect1.position, Vector2.down, .01f);
+        RaycastHit2D groundInfo2 = Physics2D.Raycast(groundDetect2.position, Vector2.down, .01f);
 
-        if (groundInfo.collider == false) //trying to set only linear movement if on ground
+        Debug.DrawLine(transform.position, wallDetect.position, Color.green);
+        Debug.DrawLine(transform.position, groundDetect1.position, Color.red);
+        Debug.DrawLine(transform.position, groundDetect2.position, Color.red);
+
+        if (groundInfo1.collider == true || groundInfo2.collider == true) //trying to set only linear movement if on ground
         {
+            //Debug.Log("Tomato On Ground");
             transform.Translate(Vector2.right * walk_speed * Time.deltaTime);
+        }
+        else
+        {
+            Debug.Log("Tomato Off Ground");
+            transform.Translate(Vector2.down * walk_speed * Time.deltaTime);
         }
 
         if (wallInfo.collider == true) //detects wall collission
