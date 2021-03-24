38ed0ed0e1e46fc4192a9063a834d5cc208bcef1
Nathaniel Palmer <61099489+napomj@users.noreply.github.com>
2021-03-24 15:27:07 -0700

    tests

---
+++ Spicy/Assets/Tests/Nathaniel/Stress/cross_boundary.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class cross_boundary
+    {
+
+        [UnityTest]
+        public IEnumerator left()
+        {
+
+            yield return 100;
+
+            GameObject tomato = GameObject.Find("Tomato_Enemy");
+            Assert.That(tomato.transform.position.x < -9.75);
+        }
+        [UnityTest]
+        public IEnumerator right()
+        {
+
+            yield return 100;
+
+            GameObject tomato = GameObject.Find("Tomato_Enemy");
+            Assert.That(tomato.transform.position.x > 27.75);
+        }
+    }
+}
