7af5d8f885abbc4342b00881dbd1dbb795a5bdc9
David Waters <davidj2012@gmail.com>
2021-03-24 20:55:36 -0700

    Fixed the issue on GameManager not existing in current context
    Fix: add to your assembly definition within the folder containing the script referencing the GameManager a reference to my assembly "DWScripts" inside of your assembly reference.

---
+++ Spicy/Assets/Tests/David/DWStress/spawn_too_many_enemies.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class spawn_too_many_enemies
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void spawn_too_many_enemiesSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator spawn_too_many_enemiesWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
