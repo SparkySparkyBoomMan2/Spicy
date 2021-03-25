dfaa18ab8398a8067456c9f6bde93a24f88dfe5d
Nathaniel Palmer <61099489+napomj@users.noreply.github.com>
2021-03-25 03:20:51 -0700

    added edit test

---
+++ Spicy/Assets/Tests/Nathaniel/NPEditMode/Tomato_Speed.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class Tomato_Speed
+    {
+        [Test]
+        public void isSpeed1f()
+        {
+            bool isSpeedTrue = false;
+            GameObject tomato = GameObject.FindWithTag("Enemy"); //grabs tomato object
+            if(tomato.GetComponent<movement_tomato>().walk_speed == 1f)
+            {
+                isSpeedTrue = true;
+            }
+
+            Assert.IsTrue(isSpeedTrue);
+        }
+    }
+}
