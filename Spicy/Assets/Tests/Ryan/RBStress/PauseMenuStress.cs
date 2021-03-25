8c6e323ce5edecd00f7a2058586f48e169d83b79
gellyboi <78063792+gellyboi@users.noreply.github.com>
2021-03-24 20:58:11 -0700

    made duplicate of level 1 as "DuplicateOfLevel1"

---
+++ Spicy/Assets/Tests/Ryan/RBStress/PauseMenuStress.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+
+namespace Tests
+{
+    public class PauseMenuStress
+    {
+        //Load the test scene
+        /*[OneTimeSetUp]
+        public void LoadScene()
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+        */
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator PauseMenuStressWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
