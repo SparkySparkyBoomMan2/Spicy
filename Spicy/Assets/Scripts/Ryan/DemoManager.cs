7099c96f9cda1f4b535f4120b2b2fe204d61b185
gellyboi <78063792+gellyboi@users.noreply.github.com>
2021-03-29 17:50:40 -0700

    added Astar pathfinding package, and start to demo mode

---
+++ Spicy/Assets/Scripts/Ryan/DemoManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoManager : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager demoPlayer = this.GetComponent<GameManager>();
+        demoPlayer.PlayGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if key is pressed, stop demo.
+        if (Input.anyKey)
+        {
+            //Switch State to main menu
+            StopDemo();
+        }
+        //else, continue.
+    }
+
+    void StopDemo()
+    {
+        //when any button pushed, stop demo and revert to main menu
+
+    }
+}
