8252a74c67a29035a37889d654e330d595eba5cf
David Waters <davidj2012@gmail.com>
2021-03-29 20:57:50 -0700

    Starting work on backgrounds

---
+++ Spicy/Assets/Scripts/David/InfiniteBackgroundScrolling.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfiniteBackgroundScrolling : MonoBehaviour
+{
+    private float xLength, xStartPosition;
+    private float yLength, yStartPosition;
+    public float speed;
+
+    void Start()
+    {
+        xStartPosition = transform.position.x;
+        xLength = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = new Vector2(Mathf.Repeat(Time.deltaTime, speed), transform.position.y);
+
+
+
+        
+    }
+}
