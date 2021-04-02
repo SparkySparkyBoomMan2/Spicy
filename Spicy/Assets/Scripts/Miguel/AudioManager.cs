2eb38f20c5de024cb55fc553ae98a4ffe9c14047
SparkySparkyBoomMan2 <78051773+SparkySparkyBoomMan2@users.noreply.github.com>
2021-04-01 23:50:45 -0700

    Added Audio Manager for music
    Added folders for audio as well, with songs I chose for future levels

---
+++ Spicy/Assets/Scripts/Miguel/AudioManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    public static AudioManager instance;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        // Singleton pattern here
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        //DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+        
+    }
+
+    void Start()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+
+        if (sceneName == "MainMenu")
+            Play("Flamenco1");
+        else if (sceneName == "Level 1")
+            Play("Flamenco2");
+        //Play("");        
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound" + name + "not found!");
+            return;
+        }
+        s.source.Play();
+    }
+}
