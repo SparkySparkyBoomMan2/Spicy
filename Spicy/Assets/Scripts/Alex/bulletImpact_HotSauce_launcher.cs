7cda274bec89a2f95f17269b2a3d6c91be33bf4a
alexpena5635 <alexpena5635@gmail.com>
2021-04-08 19:52:02 -0700

    Added fire effect to grenade impact for weapon
    I made a special particle effect for the grenade launcher that looks somewhat like fire, and kills any enemy that enters the "fire".

--- Spicy/Assets/Scripts/Alex/bulletImpact_HotSauce_launcher.cs
+++ Spicy/Assets/Scripts/Alex/bulletImpact_HotSauce_launcher.cs
@@ -4,10 +4,34 @@ using UnityEngine;
 
 public class bulletImpact_HotSauce_launcher : bulletImpact_default
 {
-    //void Start()
-    //{
+    public GameObject bullet;
+    
+    void Start()
+    {
         //Destroys the particle system after it plays
         //may need to change the second parameter if particle system plays for longer or shorter
-        //Destroy(gameObject, 2.5f);
-    //}
+        //Debug.Log("We are here!!!! YAYAYAYAYAY");
+        Destroy(gameObject, 2.5f);
+    }
+
+    void OnParticleCollision(GameObject other)
+    {      
+           //Debug.Log("We are here!!!! YAYAYAYAYAY");
+            
+           DamageEnemy(other);
+    }
+
+    void DamageEnemy(GameObject other)
+    {
+        death_tomato enemy = other.GetComponent<death_tomato>();
+        
+        //Debug.Log("We are here!!!! YAYAYAYAYAY --- " + other.tag);
+        if (enemy != null)
+        {
+            Debug.Log("We are here!!!! --- " + enemy.name);
+            
+            other.GetComponent<CircleCollider2D>().enabled = false;
+            enemy.Die();
+        }
+    }
 }
--- Spicy/Assets/Scripts/Alex/bulletImpact_default.cs
+++ Spicy/Assets/Scripts/Alex/bulletImpact_default.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class bulletImpact_default : MonoBehaviour
 {
+   
+
     void Start()
     {
         //Destroys the particle system after it plays
         //may need to change the second parameter if particle system plays for longer or shorter
+        //part = GetComponent<ParticleSystem>();
+        //collisionEvents = new List<ParticleCollisionEvent>();
         Destroy(gameObject, 2.5f);
     }
 
+   
 }
