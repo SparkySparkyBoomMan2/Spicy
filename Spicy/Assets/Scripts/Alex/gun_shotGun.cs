8328da7b59b6a94748506e210d84e262d3418323
alexpena5635 <alexpena5635@gmail.com>
2021-04-20 13:59:10 -0700

    Added shotgun to game

--- Spicy/Assets/Scripts/Alex/gun_shotGun.cs
+++ Spicy/Assets/Scripts/Alex/gun_shotGun.cs
@@ -14,11 +14,22 @@ public class gun_shotGun : gun_default
         AllowFire = false;
         animator.SetBool("isFiring", true);
 
+        //Get the rotation of the child object (which is the fire point)
         Vector3 rot = transform.GetChild(0).rotation.eulerAngles;
         rot = new Vector3(rot.x, rot.y, rot.z + 180);
-        //rot = new Vector3(rot.x, rot.y, rot.z);
 
-        Instantiate(bullet, new Vector2 (transform.GetChild(0).position.x, transform.GetChild(0).position.y), Quaternion.Euler(rot));
+        //Determines the direction each bullet from the shotgun will take, relative to straightforward
+        Vector3 rotUp = new Vector3(rot.x, rot.y, rot.z + 15);
+        Vector3 rotDown = new Vector3(rot.x, rot.y, rot.z - 15);
+        Vector3 rotUp2 = new Vector3(rot.x, rot.y, rot.z + 30);
+        Vector3 rotDown2 = new Vector3(rot.x, rot.y, rot.z - 30);
+     
+
+        //Instantiates each bullet, acting like spread from a shotgun pellet in this case
+        Instantiate(bullet, new Vector2 (transform.GetChild(0).position.x, transform.GetChild(0).position.y), Quaternion.Euler(rotDown));
+        Instantiate(bullet, new Vector2 (transform.GetChild(0).position.x, transform.GetChild(0).position.y), Quaternion.Euler(rotUp));
+        Instantiate(bullet, new Vector2 (transform.GetChild(0).position.x, transform.GetChild(0).position.y), Quaternion.Euler(rotDown2));
+        Instantiate(bullet, new Vector2 (transform.GetChild(0).position.x, transform.GetChild(0).position.y), Quaternion.Euler(rotUp2));
         
         yield return new WaitForSeconds(FireRateDelay);
         animator.SetBool("isFiring", false);
