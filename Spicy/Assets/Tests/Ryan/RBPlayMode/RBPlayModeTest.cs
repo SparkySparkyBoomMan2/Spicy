0d0f67dc64623c857784ed9549121ee47d132716
gellyboi <78063792+gellyboi@users.noreply.github.com>
2021-04-23 06:16:16 -0700

    added play mode test

--- Spicy/Assets/Tests/Ryan/RBPlayMode/RBPlayModeTest.cs
+++ Spicy/Assets/Tests/Ryan/RBPlayMode/RBPlayModeTest.cs
@@ -3,26 +3,40 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
 
 namespace Tests
 {
     public class RBPlayModeTest
     {
-        // A Test behaves as an ordinary method
-        [Test]
-        public void RBPlayModeTestSimplePasses()
+        [OneTimeSetUp]
+        public void LoadScene()
         {
-            // Use the Assert class to test conditions
+            SceneManager.LoadScene("RBGhostTestScene");
         }
 
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
         // `yield return null;` to skip a frame.
         [UnityTest]
-        public IEnumerator RBPlayModeTestWithEnumeratorPasses()
+        public IEnumerator RBPlayModeTestForBaseGhost()
         {
+            //GameObject prefabGhost = GameObject.Instantiate(Resources.Load("Assets/Prefabs/Ryan/GhostEnemyNew.prefab") as GameObject);
+            GameObject prefabGhost = GameObject.Find("GhostEnemyNew");
+            yield return 60;
             // Use the Assert class to test conditions.
+            Assert.IsNotNull(prefabGhost.GetComponent<GhostMasterAI>().playerLoc);
             // Use yield to skip a frame.
-            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator RBPlayModeTestForPassiveGhost()
+        {
+            //GameObject prefabPGhost = GameObject.Instantiate(Resources.Load("Assets/Prefabs/Ryan/PassiveGhostEnemy.prefab") as GameObject);
+            GameObject prefabPGhost = GameObject.Find("PassiveGhostEnemy");
+            yield return 60;
+            // Use the Assert class to test conditions.
+            Assert.IsNotNull(prefabPGhost.GetComponent<PassiveGhost>().playerLoc);
+            // Use yield to skip a frame.
         }
     }
 }
